1e4900ba5f6d27d2afb7cf419edb330b411917be
noesnestor <nestor.marvi@gmail.com>
2023-06-27 01:35:16 -0300

    Corregido imports

--- Scripts/DatosCsv.py
+++ Scripts/DatosCsv.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 names = ['ID', 'anio', 'mes', 'Sexo', 'Edad', 'region','PEA','Desempleo','Salario']
 url = "Datos\ECH_2022 - BD Proyecto Final PyE 2023.csv"
 ech = pd.read_csv(url, names=names, delimiter=";",skiprows=1)
