a3252acf894ed21b88e0984ab931b9833beea2b4
noesnestor <nestor.marvi@gmail.com>
2023-07-01 04:41:37 -0300

    Ejercicio 2 estadística completado

--- Scripts/DatosCsv.py
+++ Scripts/DatosCsv.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 names = ['ID', 'anio', 'mes', 'Sexo', 'Edad', 'region','PEA','Desempleo','Salario']
 url = "Datos\ECH_2022 - BD Proyecto Final PyE 2023.csv"
-ech = pd.read_csv(url, names=names, delimiter=";",skiprows=1)
+ech_unfiltered = pd.read_csv(url, names=names, delimiter=";",skiprows=1)
+ech_unfiltered["Salario"] = pd.to_numeric(ech_unfiltered["Salario"],'coerce')
+ech = ech_unfiltered.dropna()
