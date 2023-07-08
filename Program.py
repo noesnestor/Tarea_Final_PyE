792ea4cfcdb4381a513ec96fa0f3e05cd021bfc3
noesnestor <nestor.marvi@gmail.com>
2023-07-08 18:53:15 -0300

    moved program

---
+++ Program.py
@@ -0,0 +1,6 @@
+import Scripts.EstadisticaDescriptiva.DesempleoEstadisticaDescriptiva as a1
+import Scripts.EstadisticaDescriptiva.SalarioEstadisticaDescriptiva as a2
+import Scripts.Estimacion.Estimacion as b
+import Scripts.PruebaHipotesis.PruebaHipotesisDesempleo as c1
+import Scripts.PruebaHipotesis.PruebaHipotesisSalario as c2
+
