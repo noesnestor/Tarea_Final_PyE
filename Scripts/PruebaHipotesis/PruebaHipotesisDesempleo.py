8088dad00f805f2d09e0e8a7303d08d635160303
noesnestor <nestor.marvi@gmail.com>
2023-07-08 20:29:11 -0300

    Removido datos no usados

--- Scripts/PruebaHipotesis/PruebaHipotesisDesempleo.py
+++ Scripts/PruebaHipotesis/PruebaHipotesisDesempleo.py
@@ -1,24 +1,4 @@
-import sys
-sys.path.append("Scripts")
-from DatosCsv import ech
-import pandas as pd
 import scipy.stats as st
-
-
-def estimarDesempleoTotal(datos : pd.DataFrame) -> float: #revisar
-
-    suma_desempleados = len(datos[(datos['Desempleo'] == 1) & (datos['PEA'] == 1)])
-    suma_pea = len(datos[(datos['PEA'] == 1)])
-
-    desempleados_muestra = suma_desempleados / suma_pea
-
-    pea_total = 1757161
-    des_total =  pea_total * desempleados_muestra
-
-    return des_total
-
-
-estim = estimarDesempleoTotal(ech)
 
 testT = st.ttest_1samp(0.0761, 0.07, alternative='greater')
 print(testT.pvalue) # La diferencia es tan baja que scipy pierde precisión y otorga NaN
